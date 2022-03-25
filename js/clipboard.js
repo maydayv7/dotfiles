e222bcb4bd46d7f7e8a721a11c3fcf396ef21342
maydayv7 <maydayv7@users.noreply.github.com>
2022-03-25 14:05:46 +0000

    deploy: 3827cf06e24c6cc6e5dbfab9c43a826d8ade9846

---
+++ js/clipboard.js
@@ -0,0 +1,44 @@
+function addCopyButtons(clipboard) {
+  document.querySelectorAll("pre > code").forEach(function (codeBlock) {
+    var button = document.createElement("button");
+    button.className = "copy";
+    button.type = "button";
+    button.innerText = "📋 Copy";
+    button.addEventListener("click", function () {
+      clipboard.writeText(codeBlock.innerText).then(
+        function () {
+          button.blur();
+          button.innerText = "Copied!";
+          setTimeout(function () {
+            button.innerText = "📋 Copy";
+          }, 2000);
+        },
+        function (error) {
+          button.innerText = "Error";
+        }
+      );
+    });
+
+    var pre = codeBlock.parentNode;
+    if (pre.parentNode.classList.contains("highlight")) {
+      var highlight = pre.parentNode;
+      highlight.parentNode.insertBefore(button, highlight);
+    } else {
+      pre.parentNode.insertBefore(button, pre);
+    }
+  });
+}
+
+if (navigator && navigator.clipboard) {
+  addCopyButtons(navigator.clipboard);
+} else {
+  var script = document.createElement("script");
+  script.src =
+    "https://cdnjs.cloudflare.com/ajax/libs/clipboard-polyfill/2.7.0/clipboard-polyfill.promise.js";
+  script.integrity = "sha256-waClS2re9NUbXRsryKoof+F9qc1gjjIhc2eT7ZbIv94=";
+  script.crossOrigin = "anonymous";
+  script.onload = function () {
+    addCopyButtons(clipboard);
+  };
+  document.body.appendChild(script);
+}
