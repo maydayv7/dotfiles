dd4c936af681336d8f70068970666f8a14f7c836
maydayv7 <maydayv7@gmail.com>
2024-03-01 10:15:47 +0530

    chore(packages): Update `firefox-gnome-theme`
    7c372d58b61b56b1e4890f25ee99979eb5c53a7c ---> 4e966509c180f93ba8665cd73cad8456bf44baab

--- packages/firefox-gnome-theme/metadata.nix
+++ packages/firefox-gnome-theme/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo = "https://github.com/rafaelmardojai/firefox-gnome-theme";
   branch = "master";
-  rev = "7c372d58b61b56b1e4890f25ee99979eb5c53a7c";
-  sha256 = "sha256-3/vqVpPkzJIZToPFRqUstMGBGtPq52qUdt2VMBEG80w=";
+  rev = "4e966509c180f93ba8665cd73cad8456bf44baab";
+  sha256 = "sha256-gIBZCPB0sA8Gagrxd8w4+y9uUkWBnXJBmq9Ur5BYTQU=";
 }
